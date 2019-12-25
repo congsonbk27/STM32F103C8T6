3c40281eb0519e069731ee27ff51daf6db4f439b
congsonbk27 <congsonbk27@gmail.com>
2019-12-25 09:54:46 +0700

    first pull code

---
+++ miniblink/miniblink.d
@@ -0,0 +1,19 @@
+miniblink.o: miniblink.c \
+ /home/son/stm32/libopencm3/include/libopencm3/stm32/rcc.h \
+ /home/son/stm32/libopencm3/include/libopencm3/cm3/common.h \
+ /opt/gcc-arm/lib/gcc/arm-none-eabi/8.2.1/include/stdint.h \
+ /opt/gcc-arm/arm-none-eabi/include/stdint.h \
+ /opt/gcc-arm/arm-none-eabi/include/machine/_default_types.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/features.h \
+ /opt/gcc-arm/arm-none-eabi/include/_newlib_version.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/_intsup.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/_stdint.h \
+ /opt/gcc-arm/lib/gcc/arm-none-eabi/8.2.1/include/stdbool.h \
+ /home/son/stm32/libopencm3/include/libopencm3/stm32/memorymap.h \
+ /home/son/stm32/libopencm3/include/libopencm3/stm32/f1/memorymap.h \
+ /home/son/stm32/libopencm3/include/libopencm3/cm3/memorymap.h \
+ /home/son/stm32/libopencm3/include/libopencm3/stm32/f1/rcc.h \
+ /home/son/stm32/libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ /home/son/stm32/libopencm3/include/libopencm3/stm32/gpio.h \
+ /home/son/stm32/libopencm3/include/libopencm3/stm32/f1/gpio.h \
+ /home/son/stm32/libopencm3/include/libopencm3/stm32/common/gpio_common_all.h
---
+++ uarthwfc/uarthwfc.d
@@ -0,0 +1,24 @@
+uarthwfc.o: uarthwfc.c miniprintf.h \
+ /opt/gcc-arm/lib/gcc/arm-none-eabi/8.2.1/include/stdarg.h \
+ /home/son/stm32/libopencm3/include/libopencm3/stm32/rcc.h \
+ /home/son/stm32/libopencm3/include/libopencm3/cm3/common.h \
+ /opt/gcc-arm/lib/gcc/arm-none-eabi/8.2.1/include/stdint.h \
+ /opt/gcc-arm/arm-none-eabi/include/stdint.h \
+ /opt/gcc-arm/arm-none-eabi/include/machine/_default_types.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/features.h \
+ /opt/gcc-arm/arm-none-eabi/include/_newlib_version.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/_intsup.h \
+ /opt/gcc-arm/arm-none-eabi/include/sys/_stdint.h \
+ /opt/gcc-arm/lib/gcc/arm-none-eabi/8.2.1/include/stdbool.h \
+ /home/son/stm32/libopencm3/include/libopencm3/stm32/memorymap.h \
+ /home/son/stm32/libopencm3/include/libopencm3/stm32/f1/memorymap.h \
+ /home/son/stm32/libopencm3/include/libopencm3/cm3/memorymap.h \
+ /home/son/stm32/libopencm3/include/libopencm3/stm32/f1/rcc.h \
+ /home/son/stm32/libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ /home/son/stm32/libopencm3/include/libopencm3/stm32/gpio.h \
+ /home/son/stm32/libopencm3/include/libopencm3/stm32/f1/gpio.h \
+ /home/son/stm32/libopencm3/include/libopencm3/stm32/common/gpio_common_all.h \
+ /home/son/stm32/libopencm3/include/libopencm3/stm32/usart.h \
+ /home/son/stm32/libopencm3/include/libopencm3/stm32/f1/usart.h \
+ /home/son/stm32/libopencm3/include/libopencm3/stm32/common/usart_common_f124.h \
+ /home/son/stm32/libopencm3/include/libopencm3/stm32/common/usart_common_all.h
